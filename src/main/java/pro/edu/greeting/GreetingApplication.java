78e5a084c357cc3469b26f4e1440e3dc40096bd1
gprohorov <yuramail222@gmail.com>
2020-04-20 20:28:27 +0300

    Initial commit

---
+++ src/main/java/pro/edu/greeting/GreetingApplication.java
@@ -0,0 +1,13 @@
+package pro.edu.greeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/edu/greeting/GreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.edu.greeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
